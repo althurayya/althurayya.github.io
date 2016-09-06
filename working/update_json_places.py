6cd2df26b9cf6f71cf5ae08a34ba81ad9cabeeba
Maxim Romanov <romanov.maxim@gmail.com>
2016-09-06 22:06:37 +0200

    First round of results merged into places -- cool

--- working/update_json_places.py
+++ working/update_json_places.py
@@ -1,7 +1,22 @@
-import json, codecs, pprint
+import json, codecs, pprint, fuzzywuzzy, sys
+
+from fuzzywuzzy import fuzz
+
+sys.path.append("Z:/Documents/c.GitProjects/PythonFunctions")
+sys.path.append("/Users/romanov/Documents/c.GitProjects/PythonFunctions")
+import mgr
 
 srcFile = "/Users/romanov/Documents/c.GitProjects/althurayya.github.io/master/places.geojson"
-trgFile = "/Users/romanov/Documents/c.GitProjects/althurayya.github.io/master/places_test.geojson"
+trgFile = "/Users/romanov/Documents/c.GitProjects/althurayya.github.io/master/places.geojson"
+
+def loadCSVdata(file):
+    with open(file, "r", encoding="utf8") as f:
+        data = f.read()
+        data = mgr.normalizeArabic(data)
+        data = data.split("\n")
+    return(data)
+
+checkData = loadCSVdata("/Users/romanov/Documents/c.GitProjects/althurayya.github.io/working/_comparisonData")
 
 def loadJSON(file):
     with open(srcFile) as json_data:
@@ -11,6 +26,34 @@ def loadJSON(file):
             #for k,v in f["properties"].items():
             #print(f["properties"])
             del f["properties"]["textual_sources_uris"]
+
+            if "sources_arabic" not in f["properties"]:
+                f["properties"]["sources_arabic"] = {}
+            if "sources_english" not in f["properties"]:
+                f["properties"]["sources_english"] = {}
+
+            testline = f["properties"]["cornuData"]["toponym_arabic"]+"، "+ \
+                       f["properties"]["cornuData"]["toponym_arabic_other"]
+
+            testline = mgr.normalizeArabic(testline)
+            testline = list(set(testline.replace("، ", "،").split("،")))
+
+            for t in testline:
+                for c in checkData:
+                    c = c.split("\t")
+                    if t == c[2]:
+                        if c[0] not in f["properties"]["sources_arabic"]:
+                            f["properties"]["sources_arabic"][c[0]] = [c[0], 100, "na"]
+                    else:
+                        test = fuzz.ratio(t, c[2])
+                        if test >= 90:
+                            if c[0] not in f["properties"]["sources_arabic"]:
+                                f["properties"]["sources_arabic"][c[0]] = [c[0], test, "na"]                            
+                            #print("%s > %s (%d)" % (t, c[2], fuzz.ratio(t, c[2])))
+                            #input()
+                    #input()
+
+            
             #print(f["properties"])
             #input()
 
