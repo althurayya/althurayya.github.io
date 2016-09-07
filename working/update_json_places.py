950877ed8279d9763788c981d0d6c3efff6bffdf
Maxim Romanov <romanov.maxim@gmail.com>
2016-09-07 17:45:55 +0200

    Sources data regernerated

--- working/update_json_places.py
+++ working/update_json_places.py
@@ -16,7 +16,7 @@ def loadCSVdata(file):
         data = data.split("\n")
     return(data)
 
-# should not rerun texts that have been already added!
+# should not rerun texts that have been already added: generate individual for each source!
 checkData = loadCSVdata("/Users/romanov/Documents/c.GitProjects/althurayya.github.io/working/_comparisonData")
 
 def loadJSON(file):
@@ -26,21 +26,29 @@ def loadJSON(file):
         d = json.load(json_data)
         
         for f in d["features"]:
-            #for k,v in f["properties"].items():
-            #print(f["properties"])
             if "textual_sources_uris" in f["properties"]:
                 del f["properties"]["textual_sources_uris"]
+                
+
+##            if "sources_arabic" in f["properties"]:
+##                del f["properties"]["sources_arabic"]
+##            if "sources_english" in f["properties"]:
+##                del f["properties"]["sources_english"]
+                
 
             if "sources_arabic" not in f["properties"]:
                 f["properties"]["sources_arabic"] = {}
             if "sources_english" not in f["properties"]:
                 f["properties"]["sources_english"] = {}
 
+
             testline = f["properties"]["cornuData"]["toponym_arabic"]+"، "+ \
                        f["properties"]["cornuData"]["toponym_arabic_other"]
 
+
             testline = mgr.normalizeArabic(testline)
             testline = list(set(testline.replace("، ", "،").split("،")))
+
 
             ls = []
             for t in testline:
@@ -53,11 +61,14 @@ def loadJSON(file):
                     #input(ls)
             ls = sorted(ls, reverse=True)
 
+
             for i in ls:
                 if i[1] not in f["properties"]["sources_arabic"]:
                     if i[0] >= 70:
+                        #input(i)
                         added += 1
                         f["properties"]["sources_arabic"][i[1]] = [i[0], i[2], "na"]
+
 
         with open(trgFile,"w",encoding='utf-8') as fp:
             json.dump(d,fp,sort_keys=True, indent=4,ensure_ascii=False)
@@ -66,25 +77,5 @@ def loadJSON(file):
         print("Total added: %s" % "{:,}".format(added))
 
 loadJSON(srcFile)
-
-
-##t1 = [50, "Test01", "bla"]
-##t2 = [90, "Test02", "bla"]
-##t3 = [1, "Test03", "bla"]
-##t4 = [4, "Test04", "bla"]
-##
-##li = [t1, t4, t2, t3]
-##
-##print(li)
-##
-##print(sorted(li))
-##print(sorted(li, reverse=True))
-
-##t1 = "قلعة رباه"
-##t2 = "قلعه كيانه"
-##print(t1)
-##print(t2)
-##print(fuzz.ratio(t1, t2))
-
 
 print("Tada!")
