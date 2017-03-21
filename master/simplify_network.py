de060f417794e6ba92c439f741d8e679b7b7371c
Maxim Romanov <romanov.maxim@gmail.com>
2017-03-21 09:36:41 +0100

    full routes are back

--- master/simplify_network.py
+++ master/simplify_network.py
@@ -1,15 +1,43 @@
 import json
 
-with open("routes.json", "r", encoding="utf8") as f1:
+with open("routes_full.json", "r", encoding="utf8") as f1:
     routes = json.loads(f1.read())
 
+    nodesWeight = {}
+    edgeDic = {}
+    
+
 for i in routes["features"]:
-    i["geometry"]["coordinates"] = [i["geometry"]["coordinates"][0], i["geometry"]["coordinates"][-1]]
+    edgeKey = i["properties"]["sToponym"]+"@"+i["properties"]["eToponym"]
+    if edgeKey in edgeDic:
+        print("%s is already in the dictionary" % edgeKey)
+        print(edgeDic[edgeKey])
+        print(len(edgeDic))
+        print("===ERROR===")
+    else:
+        edgeDic[edgeKey] = i
+
+        
+    #input(edgeKey)
     #input(i)
+    i["geometry"]["coordinates"] = [i["geometry"]["coordinates"][0], i["geometry"]["coordinates"][-1]]
+    #input(i["properties"]["sToponym"])
+
+    # count nodes' weights
+    if i["properties"]["sToponym"] in nodesWeight:
+        nodesWeight[i["properties"]["sToponym"]] += 1
+    else:
+        nodesWeight[i["properties"]["sToponym"]]  = 1
+
+    if i["properties"]["eToponym"] in nodesWeight:
+        nodesWeight[i["properties"]["eToponym"]] += 1
+    else:
+        nodesWeight[i["properties"]["eToponym"]]  = 1    
+        
     # update the coordinates
     pass    
 
-with open("places.geojson", "r", encoding="utf8") as f2:
+with open("places_full.geojson", "r", encoding="utf8") as f2:
     places = json.loads(f2.read())
 
 for i in places["features"]:
